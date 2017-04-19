218c1bfe63bf2e5320f7f59f5e89b563f2dfafce
Leo <Jean@JeandeAir.AirDream>
2017-04-19 15:13:42 +0800

    添加扫描二维码的功能

--- pairearch_WLY/RootTabBar/Home/Controllers/HomeViewController.m
+++ pairearch_WLY/RootTabBar/Home/Controllers/HomeViewController.m
@@ -10,14 +10,14 @@
 
 #import "LoginViewController.h"
 #import "PaomaLabel.h"
-#import "LatestNoticeInfoViewController.h"
 #import "WQLPaoMaView.h"
 #import "HomeTableCell.h"
 #import "HomePageModel.h"
+#import "SGScanningQRCodeVC.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface HomeViewController ()<UITableViewDelegate, UITableViewDataSource>
+@interface HomeViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *safetyCheckBtn;
 @property (weak, nonatomic) IBOutlet UIButton *userIconBtn;
 @property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *userNumberLabel;
@@ -25,11 +25,7 @@
 @property (nonatomic, strong) PaomaLabel *noticeContentL;  //通知公告栏
 @property (nonatomic, strong) WQLPaoMaView *paoma;  //公告栏；
 
-@property (nonatomic, strong) UITableView *tableView;
-
 @property (nonatomic, strong) UILabel *headLabel;
-
-@property (nonatomic, strong) NSMutableArray *dataModelArr;
 
 @end
 
@@ -40,15 +36,8 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor whiteColor];
-    self.dataModelArr = [NSMutableArray array];
     
     [self.view addSubview:self.paoma];
-    [self.view addSubview:self.tableView];
-    
-    self.safetyCheckBtn.layer.masksToBounds = YES;
-    self.safetyCheckBtn.layer.cornerRadius = 5.0;
-    self.safetyCheckBtn.layer.borderColor = [UIColor whiteColor].CGColor;
-    self.safetyCheckBtn.layer.borderWidth = 0.5;
     
     self.userIconBtn.layer.masksToBounds = YES;
     self.userIconBtn.layer.cornerRadius = (kScreenWidth * 6) / 32 / 2.0;
@@ -61,9 +50,6 @@
     
     //从后台到前台开始动画
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActiveNotificationAction) name:UIApplicationWillEnterForegroundNotification object:nil];
-    
-    //请求首页数据
-    [self getHomePageData];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -74,12 +60,6 @@
     self.userNumberLabel.text = [LoginModel shareLoginModel].tel;
     
     [self.paoma startAnimation];
-    
-    //获取数据检查状态
-    [self getIsSafetyCheck];
-    
-    //获取首页Data数据
-    [self getHomePageData];
     
 }
 
@@ -112,25 +92,6 @@
     return _paoma;
 }
 
-- (UITableView *)tableView {
-    if (!_tableView) {
-        self.tableView = [UITableView new];
-        [self.view addSubview:self.tableView];
-        [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self.paoma.mas_bottom);
-            make.left.equalTo(self.view.mas_left);
-            make.bottom.equalTo(self.view.mas_bottom);
-            make.right.equalTo(self.view.mas_right);
-        }];
-        self.tableView.delegate = self;
-        self.tableView.dataSource = self;
-        self.tableView.tableFooterView = [UITableView new];
-        self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-        self.tableView.tableHeaderView = self.headLabel;
-    }
-    return _tableView;
-}
-
 - (UILabel *)headLabel {
     if (!_headLabel) {
         self.headLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 60.0)];
@@ -142,90 +103,44 @@
     return _headLabel;
 }
 
-#pragma mark -- Get Data
-
-//检查是否进行安全检查
-- (void)getIsSafetyCheck {
-    [[NetworkHelper shareClient] GET:DAFETY_CHECK_API parameters:@{@"driverTel":[LoginModel shareLoginModel].tel? [LoginModel shareLoginModel].tel:@""} progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSDictionary *respondDict = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:nil];
-        NSInteger status = [respondDict[@"status"] integerValue];
-        if (status == 1) {
-            [self.safetyCheckBtn setTitle:@"安全检查" forState:UIControlStateNormal];
-        } else {
-            [self.safetyCheckBtn setTitle:@"安全检查！" forState:UIControlStateNormal];
-        }
-        //如果有感叹号，设置感叹号的颜色
-        if (self.safetyCheckBtn.currentTitle.length > 4) {
-            [self fuwenbenLabel:self.safetyCheckBtn.titleLabel FontNumber:self.safetyCheckBtn.titleLabel.font AndRange:NSMakeRange(4, 1) AndColor:[UIColor redColor]];
-        }
-    } failure:nil];
-}
-
-//获取首页Data数据
-- (void)getHomePageData {
-    [HomePageModel getDataWithParameters:@{@"driverTel":[LoginModel shareLoginModel].tel? [LoginModel shareLoginModel].tel:@""} endBlock:^(id model, NSError *error) {
-        NSArray *modelArr = model;
-        if (modelArr.count) {
-            self.dataModelArr = [NSMutableArray arrayWithArray:model];
-        }
-        [self.tableView reloadData];
-        if (self.dataModelArr.count) {
-            HomePageModel *model = self.dataModelArr[0];
-            if ([model.type isEqualToString:@"unabsorbed"]) {
-                self.headLabel.text = @"新单提示";
-            } else if ([model.type isEqualToString:@"distribution"]) {
-                self.headLabel.text = @"装货在途";
-            } else {
-                self.headLabel.text = @"";
-            }
-        } else {
-            self.headLabel.text = @"";
-        }
-    }];
-}
-
-//设置不同字体颜色
-- (void)fuwenbenLabel:(UILabel *)labell FontNumber:(id)font AndRange:(NSRange)range AndColor:(UIColor *)vaColor {
-    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:labell.text];
-    
-    //设置字号
-    [str addAttribute:NSFontAttributeName value:font range:range];
-    
-    //设置文字颜色
-    [str addAttribute:NSForegroundColorAttributeName value:vaColor range:range];
-    labell.attributedText = str;
-}
-
-#pragma mark -- TableViewDelegate
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.dataModelArr.count;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    HomePageModel *model = self.dataModelArr[indexPath.row];
-    CGFloat loadNameConstant = [BaseModel heightForTextString:[NSString stringWithFormat:@"负载单号：%@", model.CODE] width:(kScreenWidth - 85.0)  fontSize:16.0];
-    CGFloat toAddressConstant = [BaseModel heightForTextString:[NSString stringWithFormat:@"收货地址：%@", model.DC_ADDRESS] width:(kScreenWidth - 85.0)  fontSize:16.0];
-    CGFloat planTimeConstant = [BaseModel heightForTextString:[NSString stringWithFormat:@"预约装货时间：%@", model.PLAN_DELIVER_TIME] width:(kScreenWidth - 85.0)  fontSize:13.0];
-    CGFloat height = loadNameConstant + toAddressConstant + planTimeConstant + 45.0;
-    return height;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    HomeTableCell *cell = [HomeTableCell getCellWithTable:tableView];
-    cell.homeModel = self.dataModelArr[indexPath.row];
-    return cell;
-}
-
 #pragma mark -- ButtonAction
 
 - (IBAction)telePhoneAction:(UIButton *)sender {
     NSString *str=[NSString stringWithFormat:@"telprompt://%@", @"021-66188125"];
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str]];
+}
+
+
+/**
+ 扫描二维码按钮点击事件
+
+ @param sender 扫描按钮
+ */
+- (IBAction)scanCodeAction:(UIButton *)sender {
+    //获取摄像设备
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        __weak typeof(self) weakself = self;
+        SGScanningQRCodeVC *codeVC = [SGScanningQRCodeVC getSgscanningQRCodeVCWithResultBlock:^(NSString *scanResult) {
+            //扫描结束回调
+            NSLog(@"%@", scanResult);
+            
+        }];
+        NavigationController *naviNC = [[NavigationController alloc] initWithRootViewController:codeVC];
+        codeVC.navigationItem.leftBarButtonItem = [NavigationController getNavigationBackItemWithTarget:codeVC SEL:@selector(dismissModalViewControllerAnimated:)];
+        [weakself presentViewController:naviNC animated:YES completion:nil];
+    } else {
+        [MBProgressHUD bwm_showTitle:@"⚠️ 警告:未检测到您的摄像头, 请在真机上测试" toView:self.view hideAfter:HUD_HIDE_TIMEINTERVAL / 2.0];
+    }
+}
+
+/**
+ 自提运单响应事件
+
+ @param sender 点击的按钮
+ */
+- (IBAction)selfGetNumberAction:(UIButton *)sender {
+    NSLog(@"%@", sender.currentTitle);
 }
 
 - (void)dealloc {
---
+++ pairearch_WLY/Vender/SGQRCode/Controller/SGScanningQRCodeVC.h
@@ -0,0 +1,27 @@
+//
+//  SGScanningQRCodeVC.h
+//  SGQRCodeExample
+//
+//  Created by Sorgle on 16/8/25.
+//  Copyright © 2016年 Sorgle. All rights reserved.
+//
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+//
+//  - - 如在使用中, 遇到什么问题或者有更好建议者, 请于kingsic@126.com邮箱联系 - - - - - //
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - GitHub下载地址 https://github.com/kingsic/SGQRCode.git - - - - - - - - - //
+//
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+#import <UIKit/UIKit.h>
+
+
+typedef void(^ScanQrcodeResultBlock)(NSString *scanResult);
+
+@interface SGScanningQRCodeVC : UIViewController
+
+//初始化扫描控制器
++ (SGScanningQRCodeVC *)getSgscanningQRCodeVCWithResultBlock:(ScanQrcodeResultBlock)resultBlock;
+
+@end
